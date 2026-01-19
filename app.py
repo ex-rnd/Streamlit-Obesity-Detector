32348187d611070bb14b5a1713b966727a85d7be
James Kilonzo <jkilonzo2023@gmail.com>
2026-01-19 13:38:03 +0300

    Replaced backward slashes with forward slashes

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import streamlit as st
 import numpy as np
 
 # Load the model
-model = keras.models.load_model('.\model\model.keras')
+model = keras.models.load_model('./model/model.keras')
 
 # Load the dataframe 
-df = pd.read_csv('.\dataset\obesity_data.csv')
+df = pd.read_csv('./dataset/obesity_data.csv')
 
 # Create the Pages 
 
@@ -20,7 +20,7 @@ with st.container():
     image_col, title_col = st.columns([0.5, 2], vertical_alignment = 'center')
     
     with image_col:
-        st.image('.\images\logo.png', width = 100)
+        st.image('./images/logo.png', width = 100)
         
     with title_col:
         st.write('# **:blue[OBESITY🫃🏻 DETECTOR🔍]**')
